9f66ecfca902f6ec463da952ef5e883a4c2505b8
N4Br0kEr <57879396+N4Br0kEr@users.noreply.github.com>
2019-11-19 23:26:38 -0300

    Add files via upload

---
+++ Python.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+echo """
+____        _   _
+|  _ \ _   _| |_| |__   ___  _ __  
+| |_) | | | | __| |_ \ / _ \| |_ \ 
+|  __/| |_| | |_| | | | (_) | | | |
+|_|    \__/ |\__|_| |_|\___/|_| |_|
+       |___/ 
+#By:N4Br0kEr 
+#desec-security
+#18/11/2019
+#version: 1.2
+
+"""
+echo "[1] Dns "
+echo ""
+echo "[2] Whois "
+echo ""
+echo "[3] Scan "
+echo ""
+echo "[4] clientes "
+echo ""
+echo "[5] Dns reverso "
+echo ""
+read -p  "[#] > " var
+case $var in
+"1")
+
+python test02.py dns 
+exit
+esac
+
+case $var in
+"2")
+python test02.py whois
+exit
+
+esac
+case $var in
+"3")
+python test02.py scan
+exit
+esac
+case $var in 
+"4")
+echo ""
+echo "[1] Tcp"
+echo ""
+echo "[2] Udp"
+echo ""
+read -p "[*] > " test
+case $test in
+	"1")
+	echo ""
+	echo "Em Desenvolvimento"
+	echo ""
+	;;
+	"2")
+	echo ""
+	echo "Em Desenvolvimento "
+	echo ""
+	esac
+esac
+case $var in 
+"5")
+python test02.py dnsr
+exit
+esac
+
+
+
